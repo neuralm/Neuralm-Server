9f093183bb0a0953a422a27a48c599d8dcc33e94
Videntory <videntory@outlook.com>
2020-01-28 09:16:45 +0100

    Hotfix Merge Master

--- src/Neuralm.Services/Neuralm.Services.TrainingRoomService/Neuralm.Services.TrainingRoomService.Tests/TrainingSessionServiceTests.cs
+++ src/Neuralm.Services/Neuralm.Services.TrainingRoomService/Neuralm.Services.TrainingRoomService.Tests/TrainingSessionServiceTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
